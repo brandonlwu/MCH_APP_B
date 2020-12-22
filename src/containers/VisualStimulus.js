752c34145a88a31237c072414b863cef2419e7b9
erenkafadar <eren.kafadar@yale.edu>
2020-12-22 08:58:07 -0500

    stripes

--- src/containers/VisualStimulus.js
+++ src/containers/VisualStimulus.js
@@ -57,7 +57,7 @@ class VisualStimulus extends Component {
       for (var y = 0; y < h; y++) {
         if (this.props.showContrast) {
           //const val = (Math.abs(Math.floor((x - xOffset + l) / l) % 2) ^ Math.abs(Math.floor((y - yOffset) / l) % 2)) * 163;
-          const val = Math.abs(Math.floor(Math.sin(2 * 3.14 * (14 * x  + 14 * y)))) / 2 * 163; // diagonal stripes
+          const val = Math.abs(Math.floor((y - yOffset) / l) % 2) * 163 ; //horizontal stripes
           data[(x + y * w) * 4 + 0] = val*0.80224;
           data[(x + y * w) * 4 + 1] = 0;
           data[(x + y * w) * 4 + 2] = 0;
