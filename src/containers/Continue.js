4e93b4d5c02ef1436380cac4d8c543a1b6e08803
KaylaMorgan11 <kaylajmorgan11@gmail.com>
2024-04-24 15:33:10 -0400

    page changes

--- src/containers/Continue.js
+++ src/containers/Continue.js
@@ -43,8 +43,8 @@ class Continue extends Component {
                 <br /><br /> You will see a rating screen following each diagonal red stripes, press
                 <br /><br /> and then hold the <b> "Q"/YES </b> button or <b> "E"/NO </b> button.
                 <br /><br /> The longer you hold it down, the more certain you are of your choice.
-                <br /><br /> If you hold <b> "Q"/YES </b>  button down for a long time, you are very certain that you  <b> DO </b> hear the tone.
-                <br /><br /> If you hold <b> "E"/NO </b>  button down for a long time, you are very certain that you <b> DO NOT </b> hear the tone.
+                <br /><br /> If you hold <b> "Q"/YES </b>  button down for a long time, you are very certain that you  <b> DO </b> hear the final note of the melody.
+                <br /><br /> If you hold <b> "E"/NO </b>  button down for a long time, you are very certain that you <b> DO NOT </b> hear the final note of the melody.
                 <br /><br /> Please respond as <b> QUICKLY </b> and as <b> ACCURATELY </b> as you <b> POSSIBLY CAN </b>
                 <br /><br /><br /> PRESS "Q"/YES WHEN READY TO CONTINUE.
               </p>
--- src/containers/Continue_surprisal.js
+++ src/containers/Continue_surprisal.js
@@ -39,15 +39,15 @@ class Continue_surprisal extends Component {
             <p className="Continue_rating-text">
               Great!
               <br /><br /> You have completed the practice trials. Now onto the real test.
-              <br /><br />  It is important to note that the tone in the practice session was there to make sure
+              <br /><br />  It is important to note that the final notes in the practice sessions were there to make sure
               <br /><br />  you understood what to do, with very obvious and impossible-to-hear tones.
-              <br /><br />  The tone in the remainder of the experiment
+              <br /><br />  The final note of the melodies in the remainder of the experiment
               <br /><br />  may be very difficult to hear so you will have to listen very hard.
               <br /><br />  Sometimes it might be difficult to answer, but if you do not know, please guess.
-              <br /><br />  For this first task, you will not be asked to rate your confidence so just quickly press
+              <br /><br />  For this first task, you will not be asked to rate your confidence or fit of the note so just quickly press
               <br /><br />  and release the button as soon as the screen flashes with the diagonal red stripes.
-              <br /><br /> Press  <font size="+2">  <b> "Q"/YES </b> </font> if you <b> DO </b>hear the tone.
-              <br /><br /> Press <font size="+2"> <b> "E"/NO </b> </font> if you <b> DO NOT </b> hear the tone.
+              <br /><br /> Press  <font size="+2">  <b> "Q"/YES </b> </font> if you <b> DO </b>hear the note.
+              <br /><br /> Press <font size="+2"> <b> "E"/NO </b> </font> if you <b> DO NOT </b> hear the note.
               <br /><br /> Please respond as <b> QUICKLY </b> and as <b> ACCURATELY </b> as you <b> POSSIBLY CAN </b>
               <br /><br /> There will be scheduled breaks throughout the experiment, but please do not take a break unless instructed.
               <br /><br /><br /> PRESS "Q"/YES TO BEGIN THE EXPERIMENT.
