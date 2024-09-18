5241291ad00bd7487f82f8002d13592aa10cec25
KaylaMorgan11 <kaylajmorgan11@gmail.com>
2024-09-18 16:24:01 -0400

    Updates

---
+++ precache-manifest.b200fc15b10e79a73483e98e5a868d6a.js
@@ -0,0 +1,534 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92bb469ab36f2aa36760cb3f64928c6",
+    "url": "/ACH_APP_FU_6M/index.html"
+  },
+  {
+    "revision": "f0197645b8f4d72151ca",
+    "url": "/ACH_APP_FU_6M/static/css/main.f4ac948c.chunk.css"
+  },
+  {
+    "revision": "9052cbacda13f5dd69c7",
+    "url": "/ACH_APP_FU_6M/static/js/2.b864b133.chunk.js"
+  },
+  {
+    "revision": "ee45c4ac1e2cdbe20661beb901b1137f",
+    "url": "/ACH_APP_FU_6M/static/js/2.b864b133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0197645b8f4d72151ca",
+    "url": "/ACH_APP_FU_6M/static/js/main.f4137d37.chunk.js"
+  },
+  {
+    "revision": "024e5831305a5880533d",
+    "url": "/ACH_APP_FU_6M/static/js/runtime-main.b74b4333.js"
+  },
+  {
+    "revision": "bb93612ec273d53a8ebc3207bd030973",
+    "url": "/ACH_APP_FU_6M/static/media/1 (81).bb93612e.wav"
+  },
+  {
+    "revision": "13b5d7bf43def574e7466ed142e0a628",
+    "url": "/ACH_APP_FU_6M/static/media/10 (29).13b5d7bf.wav"
+  },
+  {
+    "revision": "862200776876c7851a0c88c0388cc759",
+    "url": "/ACH_APP_FU_6M/static/media/11 (76).86220077.wav"
+  },
+  {
+    "revision": "620c3841a4d0ab45c8b4752e6fabc587",
+    "url": "/ACH_APP_FU_6M/static/media/12 (21).620c3841.wav"
+  },
+  {
+    "revision": "91fcd9fc2c62a53663c206c1597d1ded",
+    "url": "/ACH_APP_FU_6M/static/media/13 (37).91fcd9fc.wav"
+  },
+  {
+    "revision": "38f5376d2159fb09532ef66dffa7cf7a",
+    "url": "/ACH_APP_FU_6M/static/media/14 (59).38f5376d.wav"
+  },
+  {
+    "revision": "20c181f82b240f7799988969a2c95892",
+    "url": "/ACH_APP_FU_6M/static/media/15 (77).20c181f8.wav"
+  },
+  {
+    "revision": "921bde9b19b2f7e96b66b87137561a69",
+    "url": "/ACH_APP_FU_6M/static/media/16 (07).921bde9b.wav"
+  },
+  {
+    "revision": "b02b393f76b5e386e01b6df45580b97e",
+    "url": "/ACH_APP_FU_6M/static/media/17 (02).b02b393f.wav"
+  },
+  {
+    "revision": "a55596050dfe5effaedd32ce3fc28e2e",
+    "url": "/ACH_APP_FU_6M/static/media/18 (64).a5559605.wav"
+  },
+  {
+    "revision": "bda85c47a364979ffc7b02851a66adb0",
+    "url": "/ACH_APP_FU_6M/static/media/19 (10).bda85c47.wav"
+  },
+  {
+    "revision": "60beb435ed13124869df6d0fd4a9061b",
+    "url": "/ACH_APP_FU_6M/static/media/2 (25).60beb435.wav"
+  },
+  {
+    "revision": "a036ad35bf780a08c1a66d0779cfa4fc",
+    "url": "/ACH_APP_FU_6M/static/media/20 (19).a036ad35.wav"
+  },
+  {
+    "revision": "4faa4f34ccad91f6c5a6c01faabfe8c7",
+    "url": "/ACH_APP_FU_6M/static/media/21 (17).4faa4f34.wav"
+  },
+  {
+    "revision": "ffd7920e9c0c912f13f4a313f1b8db24",
+    "url": "/ACH_APP_FU_6M/static/media/22 (50).ffd7920e.wav"
+  },
+  {
+    "revision": "bbaa9dcfcf6ce643de7a8ec70963b560",
+    "url": "/ACH_APP_FU_6M/static/media/23 (79).bbaa9dcf.wav"
+  },
+  {
+    "revision": "86c27b3db50a980aa2617b594b0f5e4c",
+    "url": "/ACH_APP_FU_6M/static/media/24 (88).86c27b3d.wav"
+  },
+  {
+    "revision": "660335dd3ba3579d3ee049513e608130",
+    "url": "/ACH_APP_FU_6M/static/media/25 (67).660335dd.wav"
+  },
+  {
+    "revision": "3c0cc27f5a1c582f9613c296869de666",
+    "url": "/ACH_APP_FU_6M/static/media/26 (35).3c0cc27f.wav"
+  },
+  {
+    "revision": "b16a93c4d36320e2d78db48273b65327",
+    "url": "/ACH_APP_FU_6M/static/media/27 (57).b16a93c4.wav"
+  },
+  {
+    "revision": "7b0fcb96d77fe1dd97558c082089d2cd",
+    "url": "/ACH_APP_FU_6M/static/media/28 (11).7b0fcb96.wav"
+  },
+  {
+    "revision": "89020f5de0d7b32deb9c51bf0082b148",
+    "url": "/ACH_APP_FU_6M/static/media/29 (33).89020f5d.wav"
+  },
+  {
+    "revision": "71810923d72586a6f6d953ad631367e5",
+    "url": "/ACH_APP_FU_6M/static/media/3 (82).71810923.wav"
+  },
+  {
+    "revision": "43ec09da1f106b8b7ade275ec310d7ed",
+    "url": "/ACH_APP_FU_6M/static/media/30 (27).43ec09da.wav"
+  },
+  {
+    "revision": "d76bacc4f195642c042cb4972e464091",
+    "url": "/ACH_APP_FU_6M/static/media/31 (42).d76bacc4.wav"
+  },
+  {
+    "revision": "e3831bbc1dbc032107c2170b96b7976e",
+    "url": "/ACH_APP_FU_6M/static/media/32 (41).e3831bbc.wav"
+  },
+  {
+    "revision": "a54792e8dabb89e783395862b6eff8a3",
+    "url": "/ACH_APP_FU_6M/static/media/33 (53).a54792e8.wav"
+  },
+  {
+    "revision": "a64afd9a2a78de83c1ed892360423bb3",
+    "url": "/ACH_APP_FU_6M/static/media/34 (08).a64afd9a.wav"
+  },
+  {
+    "revision": "42337cab2f40a036221bf7382508c721",
+    "url": "/ACH_APP_FU_6M/static/media/35 (74).42337cab.wav"
+  },
+  {
+    "revision": "525aab2c924d2a44bfcf164f603ecad6",
+    "url": "/ACH_APP_FU_6M/static/media/36 (30).525aab2c.wav"
+  },
+  {
+    "revision": "fc98c2299c33f2e75736275785f43910",
+    "url": "/ACH_APP_FU_6M/static/media/37 (15).fc98c229.wav"
+  },
+  {
+    "revision": "b047fc14e9cc2208d38dd8b2a674865f",
+    "url": "/ACH_APP_FU_6M/static/media/38 (20).b047fc14.wav"
+  },
+  {
+    "revision": "2b9d061b4840ba563bb828846ee591e9",
+    "url": "/ACH_APP_FU_6M/static/media/39 (13).2b9d061b.wav"
+  },
+  {
+    "revision": "bd5e855c9363bb074a4b5203c15ca991",
+    "url": "/ACH_APP_FU_6M/static/media/4 (69).bd5e855c.wav"
+  },
+  {
+    "revision": "069a12081928f6d30a1c01d309f81dc2",
+    "url": "/ACH_APP_FU_6M/static/media/40 (80).069a1208.wav"
+  },
+  {
+    "revision": "d011fa07eafc02af243c8870f2666dc2",
+    "url": "/ACH_APP_FU_6M/static/media/41 (34).d011fa07.wav"
+  },
+  {
+    "revision": "5391126aada9ee5e90b756bbc6b9eae6",
+    "url": "/ACH_APP_FU_6M/static/media/42 (73).5391126a.wav"
+  },
+  {
+    "revision": "3a70c7018e2e774fd67810a1e1b35fd1",
+    "url": "/ACH_APP_FU_6M/static/media/43 (51).3a70c701.wav"
+  },
+  {
+    "revision": "867e5dd9614a6e1cdd9aa9755e3d81bc",
+    "url": "/ACH_APP_FU_6M/static/media/44 (26).867e5dd9.wav"
+  },
+  {
+    "revision": "b204398fbd10964b25865f52327f16ec",
+    "url": "/ACH_APP_FU_6M/static/media/45 (38).b204398f.wav"
+  },
+  {
+    "revision": "9d76b18c9495a4b4fbfb380fc1ce7a1d",
+    "url": "/ACH_APP_FU_6M/static/media/46 (48).9d76b18c.wav"
+  },
+  {
+    "revision": "1761314266daa436f32a73c3d37bc579",
+    "url": "/ACH_APP_FU_6M/static/media/47 (71).17613142.wav"
+  },
+  {
+    "revision": "1dda51177448792d239cdf32a9f0b9c8",
+    "url": "/ACH_APP_FU_6M/static/media/48 (61).1dda5117.wav"
+  },
+  {
+    "revision": "9935143c21ec057c8952db741f442346",
+    "url": "/ACH_APP_FU_6M/static/media/49 (90).9935143c.wav"
+  },
+  {
+    "revision": "62ef111d183ad875bf9d90b135c833f9",
+    "url": "/ACH_APP_FU_6M/static/media/5 (31).62ef111d.wav"
+  },
+  {
+    "revision": "6735cd5f546db9b91b41c8e442871621",
+    "url": "/ACH_APP_FU_6M/static/media/50 (87).6735cd5f.wav"
+  },
+  {
+    "revision": "c411c8a463aa942a0336ffe59363c96c",
+    "url": "/ACH_APP_FU_6M/static/media/51 (78).c411c8a4.wav"
+  },
+  {
+    "revision": "d5847d9da3a0e2b23cba353b3d0c9354",
+    "url": "/ACH_APP_FU_6M/static/media/52 (28).d5847d9d.wav"
+  },
+  {
+    "revision": "b5f81db37c9eba2a5930b5b962f1736d",
+    "url": "/ACH_APP_FU_6M/static/media/53 (18).b5f81db3.wav"
+  },
+  {
+    "revision": "eebe9a0d3795d2a18ea751ebe5364bb7",
+    "url": "/ACH_APP_FU_6M/static/media/54 (70).eebe9a0d.wav"
+  },
+  {
+    "revision": "5e28626bcf9b97ff030a6316a1ad8332",
+    "url": "/ACH_APP_FU_6M/static/media/55 (01).5e28626b.wav"
+  },
+  {
+    "revision": "261c179c9db26d42408587a5559af517",
+    "url": "/ACH_APP_FU_6M/static/media/56 (54).261c179c.wav"
+  },
+  {
+    "revision": "1bed9e4c3f4e6e8738eb2f692c764c48",
+    "url": "/ACH_APP_FU_6M/static/media/57 (72).1bed9e4c.wav"
+  },
+  {
+    "revision": "3bbf6cb3ca7bdd843edf60c1cadcc137",
+    "url": "/ACH_APP_FU_6M/static/media/58 (85).3bbf6cb3.wav"
+  },
+  {
+    "revision": "12ba9e74c938df599c82d40d38fcc09c",
+    "url": "/ACH_APP_FU_6M/static/media/59 (32).12ba9e74.wav"
+  },
+  {
+    "revision": "57c3c0807f21c0f136b1216dcb0ac9bf",
+    "url": "/ACH_APP_FU_6M/static/media/6 (52).57c3c080.wav"
+  },
+  {
+    "revision": "ec1e3f0fa50b14e4c3737baabe562cdb",
+    "url": "/ACH_APP_FU_6M/static/media/60 (65).ec1e3f0f.wav"
+  },
+  {
+    "revision": "b44dd0af21b542dad8ba6087ccab6667",
+    "url": "/ACH_APP_FU_6M/static/media/61 (40).b44dd0af.wav"
+  },
+  {
+    "revision": "add2afb23d2b6e2a60a1c50a7d614982",
+    "url": "/ACH_APP_FU_6M/static/media/62 (60).add2afb2.wav"
+  },
+  {
+    "revision": "1e6d688e6615d49cca0578818c598c3d",
+    "url": "/ACH_APP_FU_6M/static/media/63 (04).1e6d688e.wav"
+  },
+  {
+    "revision": "682e29710d8e3e89ad2940f2cbbd5687",
+    "url": "/ACH_APP_FU_6M/static/media/64 (75).682e2971.wav"
+  },
+  {
+    "revision": "ce4c82a3ca5c019edc77d57af4e95ef4",
+    "url": "/ACH_APP_FU_6M/static/media/65 (24).ce4c82a3.wav"
+  },
+  {
+    "revision": "92714c93f2d089da8ae308a3379bd5a2",
+    "url": "/ACH_APP_FU_6M/static/media/66 (63).92714c93.wav"
+  },
+  {
+    "revision": "7a6f17a1f2ce71ce1bffd1b147e59d60",
+    "url": "/ACH_APP_FU_6M/static/media/67 (16).7a6f17a1.wav"
+  },
+  {
+    "revision": "6c451bf0a6c0310b8658ad1a07a79477",
+    "url": "/ACH_APP_FU_6M/static/media/68 (05).6c451bf0.wav"
+  },
+  {
+    "revision": "df2521990c981020ed6b2f3ec30238d3",
+    "url": "/ACH_APP_FU_6M/static/media/69 (91).df252199.wav"
+  },
+  {
+    "revision": "b15ce7cea9ffa589a072b766f4254525",
+    "url": "/ACH_APP_FU_6M/static/media/7 (36).b15ce7ce.wav"
+  },
+  {
+    "revision": "bc93a007060cf9696c85bfea1ad4141d",
+    "url": "/ACH_APP_FU_6M/static/media/70 (55).bc93a007.wav"
+  },
+  {
+    "revision": "f144d174aa8de7bc425dceb1158f91da",
+    "url": "/ACH_APP_FU_6M/static/media/71 (86).f144d174.wav"
+  },
+  {
+    "revision": "7210595bb15fb1511fa16e16281da1af",
+    "url": "/ACH_APP_FU_6M/static/media/72 (47).7210595b.wav"
+  },
+  {
+    "revision": "858cfe2f9774cfd660037224d5d9fd31",
+    "url": "/ACH_APP_FU_6M/static/media/73 (43).858cfe2f.wav"
+  },
+  {
+    "revision": "0abb20c9ea3c10b7e6649cd4d01fe503",
+    "url": "/ACH_APP_FU_6M/static/media/74 (56).0abb20c9.wav"
+  },
+  {
+    "revision": "55d67c7075482d2eb29b6cee9f80f242",
+    "url": "/ACH_APP_FU_6M/static/media/75 (62).55d67c70.wav"
+  },
+  {
+    "revision": "0f80b98f775349e9d70f8d08d504635e",
+    "url": "/ACH_APP_FU_6M/static/media/76 (84).0f80b98f.wav"
+  },
+  {
+    "revision": "fda45a1a14933bb889c2a4b75da95bf0",
+    "url": "/ACH_APP_FU_6M/static/media/77 (14).fda45a1a.wav"
+  },
+  {
+    "revision": "b443933731183a788dfaafe4005404e8",
+    "url": "/ACH_APP_FU_6M/static/media/78 (46).b4439337.wav"
+  },
+  {
+    "revision": "1aa3c96187a68a5e3361404c4f59ab4a",
+    "url": "/ACH_APP_FU_6M/static/media/79 (23).1aa3c961.wav"
+  },
+  {
+    "revision": "79e4275ed77469b5b04b52947271920e",
+    "url": "/ACH_APP_FU_6M/static/media/8 (68).79e4275e.wav"
+  },
+  {
+    "revision": "41e90969cc2e42c0922c0d18e7d9d12a",
+    "url": "/ACH_APP_FU_6M/static/media/80 (22).41e90969.wav"
+  },
+  {
+    "revision": "7c89565cdb3d4a082f7df0a788fc016d",
+    "url": "/ACH_APP_FU_6M/static/media/81 (83).7c89565c.wav"
+  },
+  {
+    "revision": "e269e35bb9998e5dbe0276ffde90265b",
+    "url": "/ACH_APP_FU_6M/static/media/82 (39).e269e35b.wav"
+  },
+  {
+    "revision": "7d5b6f0abffb92880d9cb02831655718",
+    "url": "/ACH_APP_FU_6M/static/media/83 (44).7d5b6f0a.wav"
+  },
+  {
+    "revision": "673064387b3d76a6f958b622795e476a",
+    "url": "/ACH_APP_FU_6M/static/media/84 (45).67306438.wav"
+  },
+  {
+    "revision": "7964488929e619f0b53690ed9bdfaed4",
+    "url": "/ACH_APP_FU_6M/static/media/9 (66).79644889.wav"
+  },
+  {
+    "revision": "d4cb8c69b7c814a658e47ffdbbd12337",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence1.d4cb8c69.wav"
+  },
+  {
+    "revision": "f1f8fd02e8a7027b576d775dfd9bf0b8",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence10.f1f8fd02.wav"
+  },
+  {
+    "revision": "5c627c36e94132ad54ce7317d3c20c34",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence2.5c627c36.wav"
+  },
+  {
+    "revision": "2b88961c03d9fa6edd3d6e17dc9276a9",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence3.2b88961c.wav"
+  },
+  {
+    "revision": "b7b1bdd6e8f7d350bf76934434231c78",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence4.b7b1bdd6.wav"
+  },
+  {
+    "revision": "e5cc2079dcaa859b771ba3ad0b4110e8",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence5.e5cc2079.wav"
+  },
+  {
+    "revision": "eb7e0fddc9816b4b0c38548ab56c1507",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence6.eb7e0fdd.wav"
+  },
+  {
+    "revision": "3fe2aca1a04bb0aaa514d80ae068d15c",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence7.3fe2aca1.wav"
+  },
+  {
+    "revision": "36f9a2c529f2d32d8cfbc8f1fa177bdd",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence8.36f9a2c5.wav"
+  },
+  {
+    "revision": "dfbedeff10fe3b6275ff75b142d3a447",
+    "url": "/ACH_APP_FU_6M/static/media/Confidence9.dfbedeff.wav"
+  },
+  {
+    "revision": "5dfde805163e7b0c60778cfc3759277d",
+    "url": "/ACH_APP_FU_6M/static/media/Gtone.5dfde805.wav"
+  },
+  {
+    "revision": "f8e860ca3e4f5573cf7de6d334ed09a4",
+    "url": "/ACH_APP_FU_6M/static/media/Hear1.f8e860ca.wav"
+  },
+  {
+    "revision": "ee0e411440478dec8667182c4a318b82",
+    "url": "/ACH_APP_FU_6M/static/media/Hear10.ee0e4114.wav"
+  },
+  {
+    "revision": "4eef5bbdda18427d6ac3bdd891440cb9",
+    "url": "/ACH_APP_FU_6M/static/media/Hear2.4eef5bbd.wav"
+  },
+  {
+    "revision": "e05a7c70604e33a3b37c7a474df3e163",
+    "url": "/ACH_APP_FU_6M/static/media/Hear3.e05a7c70.wav"
+  },
+  {
+    "revision": "b7094f19a1bde91c3cc6dcb9819fb7cf",
+    "url": "/ACH_APP_FU_6M/static/media/Hear4.b7094f19.wav"
+  },
+  {
+    "revision": "0889b25e015b9f64e9cbd2465facd35f",
+    "url": "/ACH_APP_FU_6M/static/media/Hear5.0889b25e.wav"
+  },
+  {
+    "revision": "d790382f15584342231234ae65afcd1e",
+    "url": "/ACH_APP_FU_6M/static/media/Hear6.d790382f.wav"
+  },
+  {
+    "revision": "b7a219d393ef8ffd4636f235b3d3e47a",
+    "url": "/ACH_APP_FU_6M/static/media/Hear7.b7a219d3.wav"
+  },
+  {
+    "revision": "b3177f10178e3309a2592b8b80f04d18",
+    "url": "/ACH_APP_FU_6M/static/media/Hear8.b3177f10.wav"
+  },
+  {
+    "revision": "767d5043d7a04a00907dea7b2e9b2c49",
+    "url": "/ACH_APP_FU_6M/static/media/Hear9.767d5043.wav"
+  },
+  {
+    "revision": "95e60fcc6986da6ee5d22cce614b11e5",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal1.95e60fcc.wav"
+  },
+  {
+    "revision": "5886a0ecaf873955d22f1fbe7dd5bef5",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal10.5886a0ec.wav"
+  },
+  {
+    "revision": "3f6844b19a08c939681fadd3b21adfda",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal2.3f6844b1.wav"
+  },
+  {
+    "revision": "e67132c24135c13f64ad572ec3563b7b",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal3.e67132c2.wav"
+  },
+  {
+    "revision": "0b8cc5773ba2d02964f3eb8acdb443e8",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal4.0b8cc577.wav"
+  },
+  {
+    "revision": "14ee189d9d6b357e758d7d02fb23adc4",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal5.14ee189d.wav"
+  },
+  {
+    "revision": "686c771a6bd0691dd74ccc365622f299",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal6.686c771a.wav"
+  },
+  {
+    "revision": "6903291e1cffc67d2296c98074780d88",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal7.6903291e.wav"
+  },
+  {
+    "revision": "62e1938e431a3576420111680c9fcce1",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal8.62e1938e.wav"
+  },
+  {
+    "revision": "9f31568bbf890f173c53213c2bebb663",
+    "url": "/ACH_APP_FU_6M/static/media/Surprisal9.9f31568b.wav"
+  },
+  {
+    "revision": "8f966a49319adbf70c49c65612e9b14c",
+    "url": "/ACH_APP_FU_6M/static/media/demoG.8f966a49.mp3"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/ACH_APP_FU_6M/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/ACH_APP_FU_6M/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/ACH_APP_FU_6M/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/ACH_APP_FU_6M/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/ACH_APP_FU_6M/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/ACH_APP_FU_6M/static/media/rating_keydown_5.34958048.png"
+  },
+  {
+    "revision": "825ce77e46c15ab6f80b7d76260820e2",
+    "url": "/ACH_APP_FU_6M/static/media/surprisal_keydown_1.825ce77e.png"
+  },
+  {
+    "revision": "9b785fe2d4ca56ee680991e64d189c92",
+    "url": "/ACH_APP_FU_6M/static/media/surprisal_keydown_2.9b785fe2.png"
+  },
+  {
+    "revision": "31e7275933228e05c25e9f9919a4ca0d",
+    "url": "/ACH_APP_FU_6M/static/media/surprisal_keydown_3.31e72759.png"
+  },
+  {
+    "revision": "f39127fa215cf32b9291aa29e342356d",
+    "url": "/ACH_APP_FU_6M/static/media/surprisal_keydown_4.f39127fa.png"
+  },
+  {
+    "revision": "2e87195097a248ed087ba0af3bc1f007",
+    "url": "/ACH_APP_FU_6M/static/media/surprisal_keydown_5.2e871950.png"
+  }
+]);
